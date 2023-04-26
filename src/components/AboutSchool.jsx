a8e7a62f67a6789f13a99e9a3d5055ad1db5ae00
LAPTOP-OOI6TMBS\ediso <ekrasniqi567@gmail.com>
2023-04-26 15:51:45 +0200

    aboutschool

---
+++ src/components/AboutSchool.jsx
@@ -0,0 +1,12 @@
+export default function AboutSchool () {
+    return(
+        <>
+        <div>
+        <h2 className="text-h2">About School</h2>
+        <p className="text-p pt-9 pb-28">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also  
+        </p>
+        <img className="rounded-3xl" src="https://www.news10.com/wp-content/uploads/sites/64/2022/08/classroom_1542034403442_61914801_ver1.0.jpg?strip=1"/>
+        </div>
+        </>
+    )
+}
