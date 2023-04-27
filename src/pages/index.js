625906fb990055d1c09da6e96c24578889793511
LAPTOP-OOI6TMBS\ediso <ekrasniqi567@gmail.com>
2023-04-27 15:39:01 +0200

    footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import AboutSchool from '@/components/AboutSchool'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
     <AboutSchool />
+    <Footer />
     </main>
   )
 }
